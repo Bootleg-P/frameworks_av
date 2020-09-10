LineageOS/android_frameworks_av

libcameraservice: force specific cam id for google face unlock
Change-Id: I07d5ee39eff3ede13811b3f9c6e617f7a3bac21e

services/camera/libcameraservice/Android.mk
@@ -104,6 +104,10 @@ ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1),true)
     LOCAL_CFLAGS += -DNO_CAMERA_SERVER
 endif
 
+ifeq ($(TARGET_FACE_UNLOCK_CAMERA_ID),)
+    LOCAL_CFLAGS += -DFACE_UNLOCK_CAMERA_ID=$(TARGET_FACE_UNLOCK_CAMERA_ID)
+endif
+
 LOCAL_MODULE:= libcameraservice
 
 include $(BUILD_SHARED_LIBRARY)

Bootleg-P/frameworks_av

services/camera/libcameraservice/Android.mk
@@ -112,6 +112,10 @@ ifeq ($(TARGET_CAMERA_NEEDS_CLIENT_INFO),true)
 LOCAL_CFLAGS += -DTARGET_NEEDS_CLIENT_INFO
 endif
 
+ifneq ($(TARGET_FACE_UNLOCK_CAMERA_ID),)
+    LOCAL_CFLAGS += -DFACE_UNLOCK_CAMERA_ID=$(TARGET_FACE_UNLOCK_CAMERA_ID)
+endif
+
 LOCAL_MODULE:= libcameraservice
 
 include $(BUILD_SHARED_LIBRARY)